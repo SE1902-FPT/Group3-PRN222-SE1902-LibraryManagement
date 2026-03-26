1f7fe6d76315e2b4e2bf242a828f849c86e16767
Nguyen Anh Quan <crossfireaz282@gmail.com>
2026-03-26 23:12:27 +0700

    Create, Detail, Edit book

---
+++ Pages/Admin/UserManagement/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Group3_SE1902_PRN222_LibraryManagement.Models;
+
+namespace Group3_SE1902_PRN222_LibraryManagement.Pages.Admin.UserManagement
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Group3_SE1902_PRN222_LibraryManagement.Models.ThuVienContext _context;
+
+        public DeleteModel(Group3_SE1902_PRN222_LibraryManagement.Models.ThuVienContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.UserId == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                User = user;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                User = user;
+                _context.Users.Remove(User);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Admin/UserManagement/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Group3_SE1902_PRN222_LibraryManagement.Models;
+
+namespace Group3_SE1902_PRN222_LibraryManagement.Pages.Admin.UserManagement
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Group3_SE1902_PRN222_LibraryManagement.Models.ThuVienContext _context;
+
+        public IndexModel(Group3_SE1902_PRN222_LibraryManagement.Models.ThuVienContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> User { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            User = await _context.Users
+                .Include(u => u.Role).ToListAsync();
+        }
+    }
+}

0d03888b5a67e1436a3e5874bedcfbef622c3ac9
Nguyen Anh Quan <crossfireaz282@gmail.com>
2026-03-26 21:43:00 +0700

    Admin Dashboard

---
+++ Pages/Admin/BookManagement/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Group3_SE1902_PRN222_LibraryManagement.Models;
+
+namespace Group3_SE1902_PRN222_LibraryManagement.Pages.Admin.BookManagement
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Group3_SE1902_PRN222_LibraryManagement.Models.ThuVienContext _context;
+
+        public DetailsModel(Group3_SE1902_PRN222_LibraryManagement.Models.ThuVienContext context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FirstOrDefaultAsync(m => m.BookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Book = book;
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/Admin/BookManagement/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Group3_SE1902_PRN222_LibraryManagement.Models;
+
+namespace Group3_SE1902_PRN222_LibraryManagement.Pages.Admin.BookManagement
+{
+    public class EditModel : PageModel
+    {
+        private readonly Group3_SE1902_PRN222_LibraryManagement.Models.ThuVienContext _context;
+
+        public EditModel(Group3_SE1902_PRN222_LibraryManagement.Models.ThuVienContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book =  await _context.Books.FirstOrDefaultAsync(m => m.BookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            Book = book;
+           ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(Book.BookId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Books.Any(e => e.BookId == id);
+        }
+    }
+}

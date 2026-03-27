7d6e92c96d42533b374665712001ba6074efac0d
Nguyen Anh Quan <crossfireaz282@gmail.com>
2026-03-27 13:25:38 +0700

    Category + Autho Admin

---
+++ Pages/Admin/CategoryManagement/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Group3_SE1902_PRN222_LibraryManagement.Models;
+
+namespace Group3_SE1902_PRN222_LibraryManagement.Pages.Admin.CategoryManagement
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Group3_SE1902_PRN222_LibraryManagement.Models.ThuVienContext _context;
+
+        public CreateModel(Group3_SE1902_PRN222_LibraryManagement.Models.ThuVienContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Category Category { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Categories.Add(Category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
